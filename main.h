016d0559f83e997663ba588718deb7590ce4b8cf
StephenAnthony8 <anthonysteve02@gmail.com>
2023-07-23 16:30:35 +0300

    reset

--- main.h
+++ main.h
@@ -4,13 +4,6 @@
 #include <stdarg.h>
 
 int _printf(const char *format, ...);
-void _outp(char *str, int length);
+void _write(char *str, int length);
 int _strlen(char *s);
-int _count(const char *format, va_list args);
-void _prstr(va_list args, const char *format);
-int num_val(int n);
-int int_count(int m);
-void num_prt(int n);
-void prt_int(int m);
-
 #endif /* MAIN_H */
