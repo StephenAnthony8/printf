4895c26532aecc10aaa0a632e3256893e401dcb1
GLetiwa <graceletiwa@gmail.com>
2023-07-26 15:52:06 +0300

    betty modified

--- _printf.c
+++ _printf.c
@@ -11,10 +11,10 @@ void print_buffer(char buffer[], int *bi);
 
 int _printf(const char *format, ...)
 {
-	int i = 0, bi = 0, len = 0;
-	int flags = 0, width = 0, precision = -1;
+	int i = 0, bi = 0;
+	/* int flags = 0, width = 0, precision = -1; */
 	va_list args;
-	char buffer[BUFF_SIZE], c;
+	char buffer[BUFF_SIZE];
 
 	va_start(args, format);
 
@@ -27,57 +27,55 @@ int _printf(const char *format, ...)
 		{
 			buffer[bi++] = format[i];
 			if (bi == BUFF_SIZE)
-				print_buffer(buffer, &bi);
+				_write(buffer, bi);
 		}
 		else
 		{
-			/* _write(buffer, bi); */
-			width = _width(format, &i, args);
-			precision = _precision(format, &i, args);
-			flags = _flags(format + i, &i);
 			i++;
 			switch (format[i])
 			{
 				case '%':
 					buffer[bi++] = '%';
-					bi++;
 					break;
 				case 'c':
-					c = va_arg(args, int);
-					buffer[bi++] = c;
+					buffer[bi++] = va_arg(args, int);
 					bi++;
 					break;
-				case 's': case 'r': case 'R':
-					bi += prints_string(args, width, precision, flags, &buffer[bi]);
+				case 's': case 'r': case 'R': case 'S':
+					bi += prints_string(args, format[i], &buffer[bi]);
 					break;
 				case 'l':case 'h':
 					bi += prints_xint(args, &format[i], &buffer[bi]);
 					i++;
 					break;
-				case 'd': case 'i':case 'x':case 'o': case 'p': case 'X':
+				case 'd':case 'i':case 'o':case 'u':case 'p':case 'x':case 'X':case 'b':
 					bi += prints_int(args, format[i], &buffer[bi]);
 					break;
 				default:
-					buffer[bi++] = '%';
-					buffer[bi++] = format[i];
+					buffer[bi] = '%';
+					buffer[++bi] = format[i];
+					bi++;
 					break;
+
 			}
 		}
 	}
-	print_buffer(buffer, &bi);
-	len += bi;
+	_write(buffer, bi);
 	va_end(args);
-	return (len);
+	return (reset_buffer(buffer, bi));
 }
 /**
- * print_buffer - Prints the contents of the buffer if it exist
+ * reset_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
  * @bi: Index at which to add next char, represents the length.
+ * Return: string length
  */
-void print_buffer(char buffer[], int *bi)
+int reset_buffer(char *buffer, int bi)
 {
-	if (*bi > 0)
-		write(1, &buffer[0], *bi);
-
-	*bi = 0;
+	while (*buffer != '\0')
+	{
+		*buffer = '\0';
+		buffer++;
+	}
+	return (bi);
 }
