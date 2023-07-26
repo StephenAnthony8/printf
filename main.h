99ccf05f2ec884415d08e0a64b37e21c7332d3e5
StephenAnthony8 <anthonysteve02@gmail.com>
2023-07-26 18:16:17 +0300

    tuanze debugging sasa

--- main.h
+++ main.h
@@ -18,28 +18,38 @@
 #define S_LONG 2
 #define S_SHORT 1
 
-
-int _printf(const char *format, ...);
-void _write(char *str, int length);
+void buffer_reset(char *buffer);
+void print_buffer(char buffer[], int *bi);
 void _itoa(int num, char *str);
-int _strlen(const char *str);
-void _strcpy(char *dest, const char *src);
 int _precision(const char *format, int *i, va_list args);
 int _width(const char *format, int *i, va_list args);
 int _flags(const char *format, int *i);
 int prints_character(const char *format, ...);
-int prints_int(va_list args, char specifier, char *buffer);
-int prints_xint(va_list args, const char *specifier, char *buffer);
-int prints_string(va_list args, int width, int precision, int flags, char *buffer);
 void print_buffer(char buffer[], int *bi);
 int _size(const char *format, int *i);
-int print_reverse(va_list args, char *str, char buffer[]);
 
-/* int conv */
+/* integer conversion */
+int b_select(char specifier);
+char *int_conv(int long value);
+char *x_hex(int num, char *str);
 int size(unsigned int long n, unsigned int long base);
 int *arr_num(unsigned int long n, unsigned int long base);
-char *int_conv(int long value);
 char *unint_conv(unsigned int long value, char specifier);
+int prints_int(va_list args, char specifier, char *buffer);
+int prints_xint(va_list args, const char *specifier, char *buffer);
+
+/* string conversion */
+int _strlen(const char *str);
+int x_string(char *str, char *buffer);
+void _strcpy(char *dest, const char *src);
+int print_reverse(char *str, char *buffer);
+int print_rot13string(char *str, char *buffer);
+int prints_string(va_list args,char flag, char *buffer);
+
+/* printf functions */
+int reset_buffer(char *buffer, int bi);
+int _printf(const char *format, ...);
+void _write(char *str, int length);
 
 
 
